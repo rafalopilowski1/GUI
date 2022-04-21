e0ac5d67e150d05562c713dadff19f3f449ca106
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-04-21 20:36:51 +0200

    2022-04-22: Zadanie 1

---
+++ 2022-04-22/src/Kolosy.java
@@ -0,0 +1,58 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Student {
+    private String name;
+    private String group;
+    private int score;
+
+    public Student(String name, String group, int score) {
+        this.name = name;
+        this.group = group;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s-%s",name,score);
+    }
+}
+
+public class Kolosy {
+    public static void main(String[] args) {
+        byte[] array = {};
+        try {
+            FileInputStream fis = new FileInputStream("scores.txt");
+            array = fis.readAllBytes();
+            fis.close();
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        String text = new String(array);
+        String[] lines = text.split("\\n");
+        Map<String, List<Student>> studentMap = Arrays.stream(lines).map((line) -> {
+            String[] details = line.split("\\s");
+            return new Student(details[0],details[1],Integer.parseInt(details[2]));
+        }).collect(Collectors.groupingBy(Student::getGroup));
+        studentMap.forEach((group, students) -> {
+            System.out.printf("Group %s: %s%n",group, Arrays.toString(students.toArray()));
+        });
+    }
+}
