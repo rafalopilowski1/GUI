54d67bf3cbfab2404f0ce275d81f76716845b004
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-04-22 16:20:38 +0200

    2022-04-22: Zadanie 1 (ulepszone)

--- 2022-04-22/src/Kolosy.java
+++ 2022-04-22/src/Kolosy.java
@@ -2,10 +2,13 @@ package src;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class StudentKolos {
     private String name;
@@ -37,22 +40,31 @@ class StudentKolos {
 }
 
 public class Kolosy {
-    public static void main(String[] args) {
-        byte[] array = {};
-        try {
-            FileInputStream fis = new FileInputStream("scores.txt");
-            array = fis.readAllBytes();
-            fis.close();
-        }
-        catch (IOException exception) {
-            exception.printStackTrace();
-        }
-        String text = new String(array);
-        String[] lines = text.split("\\n");
-        Map<String, List<StudentKolos>> studentMap = Arrays.stream(lines).map((line) -> {
-            String[] details = line.split("\\s");
-            return new StudentKolos(details[0],details[1],Integer.parseInt(details[2]));
-        }).collect(Collectors.groupingBy(StudentKolos::getGroup));
-        studentMap.forEach((group, students) -> System.out.printf("Group %s: %s%n",group, Arrays.toString(students.toArray())));
+    public static void main(String[] args) throws IOException {
+//        byte[] array = {};
+//        try {
+//            FileInputStream fis = new FileInputStream("scores.txt");
+//            array = fis.readAllBytes();
+//            fis.close();
+//        }
+//        catch (IOException exception) {
+//            exception.printStackTrace();
+//        }
+//        String text = new String(array);
+//        String[] lines = text.split("\\n");
+
+        /*Arrays.stream(lines)*/
+        Files
+                .lines(
+                    Paths.get("2022-04-22/scores.txt")
+                )
+                .map((line) -> {
+                    String[] details = line.split("\\s");
+                    return new StudentKolos(details[0], details[1], Integer.parseInt(details[2]));
+                })
+                .collect(
+                        Collectors.groupingBy(StudentKolos::getGroup)
+                )
+                .forEach((group, students) -> System.out.printf("Group %s: %s%n", group, Arrays.toString(students.toArray())));
     }
 }
