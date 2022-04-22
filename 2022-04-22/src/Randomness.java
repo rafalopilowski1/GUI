7a5ab4466d2dac94d7b8ac5183fb5b90704d2e8e
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-04-22 16:31:41 +0200

    2022-04-22: Zadanie 2 (ulepszone)

--- 2022-04-22/src/Randomness.java
+++ 2022-04-22/src/Randomness.java
@@ -9,14 +9,21 @@ public class Randomness {
     public static void main(String[] args) {
         Random r = new Random();
         final int N = 10_000_000, M = 10;
-        Stream.generate(r::nextInt)
-                .limit(N).map((random) -> {
+        Stream
+                .generate(r::nextInt)
+                .limit(N)
+                .map(
+                        (random) -> {
                             int div = random % M;
                             return Map.entry(Math.abs(div), random);
                         }
-                ).collect(Collectors.groupingBy(Map.Entry::getKey)).entrySet().forEach((integerListEntry -> {
-                    float average = (float) (integerListEntry.getValue().size()) / N;
-                    System.out.printf("%s -> %s\n", integerListEntry.getKey(), average);
-                }));
+                )
+                .collect(
+                        Collectors.groupingBy(Map.Entry::getKey)
+                )
+                .forEach((group, students) -> {
+                    float average = (float) (students.size()) / N;
+                    System.out.printf("%s -> %s\n", group, average);
+                });
     }
 }
